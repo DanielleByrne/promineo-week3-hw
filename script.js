682d07e6f0b0132fa2ebd0a1b0d30536724af9df
Danielle Byrne <DanielleByrne421@gmail.com>
2021-11-12 21:31:02 -0800

    finishing functions openclass

---
+++ script.js
@@ -0,0 +1,43 @@
+// function sumOfTwo(a, b){
+//     return a + b;
+// }
+
+// console.log(sumOfTwo(3))
+
+// function loopUntilX(num) {
+//   for (let i = 0; i < num; i++) {
+//     console.log(i);
+//   }
+
+//   return true;
+// }
+
+// console.log(loopUntilX("testing"));
+
+// function findCircumference(radius) {
+//   return 2 * Math.PI * radius;
+// }
+
+// console.log(findCircumference(5))
+
+// function checkout(cardBalance, price) {
+//   if (price > cardBalance) {
+//     return false;
+//   } else {
+//     return cardBalance - price;
+//   }
+// }
+
+
+// console.log(checkout(500, 100))
+
+// function speakFriend(password){
+//     if(password == "Mellon" ){
+//         return "Enter"
+//     } else {
+//         return "Access Denied"
+//     }
+// }
+
+
+// console.log(speakFriend("mellon"))
